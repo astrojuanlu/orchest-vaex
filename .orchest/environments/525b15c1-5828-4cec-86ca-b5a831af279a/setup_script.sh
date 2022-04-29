22667e05e0a49cbad26e7f3a47810405a29d7f72
Juan Luis Cano Rodríguez <juanlu@orchest.io>
2022-04-29 15:25:50 +0000

    Add fetching and conversion steps to pipeline

--- .orchest/environments/525b15c1-5828-4cec-86ca-b5a831af279a/setup_script.sh
+++ .orchest/environments/525b15c1-5828-4cec-86ca-b5a831af279a/setup_script.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# Install any dependencies you have in this shell script,
-# see https://docs.orchest.io/en/latest/fundamentals/environments.html#install-packages
-
-# E.g. mamba install -y tensorflow
-
+pip install -r requirements.txt
